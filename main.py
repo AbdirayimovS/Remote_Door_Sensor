c8747e0b8d590f93d5629e81f048083753da32d4
Sardor <96771858+AbdirayimovS@users.noreply.github.com>
2023-04-02 08:49:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,30 +20,26 @@ firebaseConfig = {
 firebase = pyrebase.initialize_app(firebaseConfig)
 
 db = firebase.database()
-users = db.get()
-df = pd.DataFrame(users.val(), index=[0]).T
-print(df)
-df.index = df.index.astype(int)
-last_update = df.tail(1)
-last_time_update = last_update.index[0]
-
-print(last_update)
-#
-dttime = datetime.utcfromtimestamp(last_time_update).strftime('%Y-%m-%d %H:%M:%S')
-
 
 #-----------------------------------------------------#
 def main():
+    users = db.get()
+    df = pd.DataFrame(users.val(), index=[0]).T
+    df.index = df.index.astype(int)
+    last_update = df.tail(1)
+    last_time_update = last_update.index[0]
+    dttime = datetime.utcfromtimestamp(last_time_update).strftime('%Y-%m-%d %H:%M:%S')
     st.header('Hello 🌎! Let`s check the Door Sensor Status')
     st.metric("Door Status", dttime, last_update[1])
-    if st.button('Update'):
-        st.header(dttime)
-        st.table(last_update)
-        st.balloons()
-        st.line_chart(last_update)
+    st.table(last_update)
+    st.balloons()
+    st.line_chart(last_update)
+        
+        
 
 if __name__ == '__main__':
     main()
+    time.sleep(1)
 
 
 
